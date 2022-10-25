873e194ea1b998f33bc66c6e563d9996a5a7c767
Ha Vu Hieu <hieuhv@dinoland.io>
2022-10-25 17:05:56 +0700

    them bai 7

---
+++ lesson-7/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <quiz-form />
+  </div>
+</template>
+
+<script>
+import QuizForm from "./components/QuizForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    QuizForm,
+  },
+};
+</script>
